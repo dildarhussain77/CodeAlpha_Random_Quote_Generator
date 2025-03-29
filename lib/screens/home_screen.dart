b8d6e62d54b269455a0d6daec4ae2f4d99e511ec
Dildar <dildarhussain876543@gmail.com>
2025-03-30 02:56:21 +0500

    home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,32 +10,34 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final quoteProvider = Provider.of<QuoteProvider>(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Random Quote Generator", style : TextStyle(color: Colors.white)),               
-        backgroundColor: Colors.blueAccent,        
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(20), // Adjust curve radius
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(child: const Text("Random Quote Generator", style : TextStyle(color: Colors.white))),               
+          backgroundColor: Colors.blueAccent,      
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(20), // Adjust curve radius
+            ),
           ),
+          
         ),
-        
-      ),
-      body: Center(
-        child: quoteProvider.currentQuote == null
-            ? const CircularProgressIndicator()
-            : Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  QuoteCard(quote: quoteProvider.currentQuote!),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    onPressed: () => quoteProvider.fetchNewQuote(),
-                    icon: const Icon(Icons.refresh),
-                    label: const Text("New Quote"),
-                  ),
-                ],
-              ),
+        body: Center(
+          child: quoteProvider.currentQuote == null
+              ? const CircularProgressIndicator()
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    QuoteCard(quote: quoteProvider.currentQuote!),
+                    const SizedBox(height: 20),
+                    ElevatedButton.icon(
+                      onPressed: () => quoteProvider.fetchNewQuote(),
+                      icon: const Icon(Icons.refresh),
+                      label: const Text("New Quote"),
+                    ),
+                  ],
+                ),
+        ),
       ),
     );
   }
